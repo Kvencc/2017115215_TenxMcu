538fce4f354977fa1874728855220467db131101
Kvencc <1343129535@qq.com>
2019-12-30 14:35:32 +0800

    增加按键4，key4

--- 期末课程作业--5268B实现煮水壶部分功能/prj3/code(5268B_Sop24)/keys.c
+++ 期末课程作业--5268B实现煮水壶部分功能/prj3/code(5268B_Sop24)/keys.c
@@ -21,17 +21,26 @@ void GetKeys() {
     keyValue2 = tempKeyValue2;
     tempKeyValue2 = D_keyNull;
   }
+
   if ( P_key3 == 0 ) {
     tempKeyValue3 = D_keyValue1;
   } else {
     keyValue3 = tempKeyValue3;
     tempKeyValue3 = D_keyNull;
   }
+
   if ( P_key1 == 0 ) {
     tempKeyValue1 = D_keyValue1;
   } else {
     keyValue1 = tempKeyValue1;
     tempKeyValue1 = D_keyNull;
+  }
+
+  if ( P_key4 == 0){
+    tempKeyValue4 = D_keyValue1;
+  } else{
+    keyValue4 = tempKeyValue4;
+    tempKeyValue4 = D_keyNull;
   }
   // if ( P_key4 == 0 ) {
     
